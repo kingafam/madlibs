7a1ba76e6cd0c2148de0afe6ca58fa010daa6bb4
King Afam Akatakpo <kingsuriel@gmail.com>
2023-03-17 17:26:54 +0100

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -11,4 +11,5 @@ verb1 = input("verb: ")
 verb2 = input("verb2: ")
 famous_person = input("famous_person: ")
 
-madlib = f"computer programming is so {adj}! it makes me so excited all the time because i love to {verb1}. stay hydrated and {verb2} like you are {famous_person}!"
+madlib = f"computer programming is so {adj}! it makes me so excited all the time because i love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+print(madlib)
