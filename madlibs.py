2c6c3a3004c089d33821296a1084d4fee8ed6abd
King Afam Akatakpo <kingsuriel@gmail.com>
2023-03-17 16:44:11 +0100

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,14 @@
-youtuber ="King afam"
+#youtuber ="King afam"
 
-print("subscribe to" + youtuber)
-print("subscribe to {}".format(youtuber))
-print("subscribe to{youtuber}")
+#print("subscribe to" + youtuber)
+#print("subscribe to {}".format(youtuber))
+#print(f"subscribe to {youtuber}")
+
+#the following code above shows how to put string together using an example with "subscribe to youtuber"
+
+adj = input("adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb2: ")
+famous_person = input("famous_person: ")
+
+madlib = f"computer programming is so {adj}! it makes me so excited all the time because i love to {verb1}. stay hydrated and {verb2} like you are {famous_person}!"
