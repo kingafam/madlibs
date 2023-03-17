a866a8855112391973c7a74976b9205619eef53c
King Afam Akatakpo <kingsuriel@gmail.com>
2023-03-17 16:17:24 +0100

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,10 @@
+<<<<<<< Updated upstream
 #string concentration (how to put strings together)
+=======
+#string concentration (how to put strings together)
+#suppose we want to create a string that says "subscribe to_____"
+youtuber = "" #string variable
+
+#few ways to do this
+
+>>>>>>> Stashed changes
